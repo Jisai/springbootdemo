83b6e983f2faab993d1819c7cf4b211d50879e16
songjisai <songjisai@163.com>
2022-05-03 19:21:19 +0800

    修改启动类。

--- src/main/java/com/scott/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/scott/springbootdemo/SpringbootdemoApplication.java
@@ -8,6 +8,30 @@ public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
+        System.out.println(print());
     }
 
+    private static String print(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("                   _ooOoo_\n");
+        sb.append("                  o8888888o\n");
+        sb.append("                  88\" . \"88\n");
+        sb.append("                  (| -_- |)\n");
+        sb.append("                  O\\  =  /O\n");
+        sb.append("               ____/`---'\\____\n");
+        sb.append("             .'  \\\\|     |//  `.\n");
+        sb.append("            /  \\\\|||  :  |||//  \\ \n");
+        sb.append("           /  _||||| -:- |||||-  \\ \n");
+        sb.append("           |   | \\\\\\  -  /// |   |\n");
+        sb.append("           | \\_|  ''\\---/''  |   |\n");
+        sb.append("           \\  .-\\__  `-`  ___/-. /\n");
+        sb.append("         ___`. .'  /--.--\\  `. . __\n");
+        sb.append("      .\"\" '<  `.___\\_<|>_/___.'  >'\"\".\n");
+        sb.append("     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |\n");
+        sb.append("     \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /\n");
+        sb.append("======`-.____`-.___\\_____/___.-`____.-'======\n");
+        sb.append("                   `=---='\n");
+        sb.append("...............BILLING-RATING-WEB...............\n");
+        return sb.toString();
+    }
 }
